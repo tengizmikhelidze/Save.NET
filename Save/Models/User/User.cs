c67b1782a2e5c966ca0f3377a8de3254b11c6d0a
10go <mixelidzetengizi@gmail.com>
2026-05-25 19:08:34 +0400

    init

--- Save/Models/User/User.cs
+++ Save/Models/User/User.cs
@@ -1,8 +1,11 @@
-﻿namespace Save.Models.User;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Save.Models.User;
 
 public class User
 {
     public int Id { get; set; }
+    [EmailAddress]
     public string Email { get; set; }
     
     public ICollection<Bookmark> Bookmarks { get; set; }
