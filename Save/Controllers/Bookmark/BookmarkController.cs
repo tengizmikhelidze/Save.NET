5564271dabaa9977f6cda67276225d94074288a4
10go <mixelidzetengizi@gmail.com>
2026-05-25 16:56:09 +0400

    bookmarks

---
+++ Save/Controllers/Bookmark/BookmarkController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Save.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class BookmarkController : Controller
+{
+    [HttpGet("{bookmarkId}")]
+    public IActionResult GetById(string bookmarkId)
+    {
+        return View();
+    }
+    
+    [HttpGet("/api/v1/[controller]s")]
+    public IActionResult GetAll()
+    {
+        return View();
+    }
+    
+    [HttpGet("Categories")]
+    public IActionResult GetAllCategories()
+    {
+        return View();
+    }
+    
+    [HttpGet("Category/{categoryId}")]
+    public IActionResult GetCategory(int categoryId)
+    {
+        return View();
+    }
+}
--- Save/obj/Debug/net10.0/Save.AssemblyInfo.cs
+++ Save/obj/Debug/net10.0/Save.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Save")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fb34cac6c927db2430414296eb182ee3a831a16")]
 [assembly: System.Reflection.AssemblyProductAttribute("Save")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Save")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
