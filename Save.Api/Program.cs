35f30bc55786d8ad93535f24a3c029290610715b
10GO <temikhelidze@tbcbank.ge>
2026-05-29 16:32:46 +0400

    envs

--- Save.Api/Program.cs
+++ Save.Api/Program.cs
@@ -9,12 +9,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging())
 {
     app.MapSwagger();
     app.UseSwagger();
     app.UseSwaggerUI();
     app.MapOpenApi();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
